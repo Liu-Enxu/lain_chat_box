612291ee2c9f8b339992253f812fae9204d6db4a
JayC180 <jaychen55555@gmail.com>
2025-03-25 03:23:23 -0500

    stuff

--- pc_side/audio_to_text.py
+++ pc_side/audio_to_text.py
@@ -1,4 +1,12 @@
-import subprocess
+import whisper
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
+
+model = whisper.load_model("large").to(device)
+options = whisper.DecodingOptions(language= 'zh', fp16=False)
 
 def audio_to_text(input_file):
-    return 
+    result = model.transcribe(input_file, language="zh")
+    return result["text"]
